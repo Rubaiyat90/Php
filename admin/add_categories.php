b193205d5468478a9d3c00738bf17a986bc9cfbc
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2024-09-01 16:47:21 +0600

    Category_Added

---
+++ admin/add_categories.php
@@ -0,0 +1,15 @@
+<form action="" method="post" class="mb-2" >
+    <div class="container-fluid text-center">
+        <h3>Add Category</h3><br><br>
+    </div>
+    <div class="container-fluid">
+        <div class="input-group bg-info w-90 mb-3 p-1">
+            <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+            <input type="text" class="form-control" name="category_title" placeholder="Insert Category" aria-label="insert_catgeory" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group w-10 mb-2 m-auto">
+            <button class="bg-info p-2 my-3 border-0">Add Category</button> 
+        </div>
+    </div>
+    
+</form>
--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,9 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin Dashbord</title>
+    <title>Admin Dashboard</title>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
   </head>
@@ -20,38 +22,36 @@
             </div>
         </nav>
     </div>
-    <div class="container-fluid">
-        <div class="row">
-            <div id="wrapper">
-                <div id="sidebar-wrapper">
-                    <ul class="sidebar-nav" style="margin-left:0;">
-                        <li class="sidebar-brand">
-                            <a href="#menu-toggle"  id="menu-toggle" style="margin-top:20px;float:right;" > <i class="fa fa-bars " style="font-size:20px !Important;" aria-hidden="true" aria-hidden="true"></i> 
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-sort-alpha-asc " aria-hidden="true"> </i> <span style="margin-left:10px;">All products</span>  </a>
-                        </li>
-                        <li>
-                            <a href="#"> <i class="fa fa-play-circle-o " aria-hidden="true"> </i> <span style="margin-left:10px;">Add products</span> </a>
-                        </li>
-                        <li>
-                            <a href="#"> <i class="fa fa-puzzle-piece" aria-hidden="true"> </i> <span style="margin-left:10px;">Add categories</span> </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>  
-            <div id="page-content-wrapper">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            </a>
-                            <script>
-                            $("#menu-toggle").click(function(e) {
-                                e.preventDefault();
-                                $("#wrapper").toggleClass("toggled");
-                            });
-                            </script>
-                        </div>
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                        Admin Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="view_products.php">Home</a>
+                </li>
+                <li>
+                    <a href="index.php?add_products">Add products</a>
+                </li>
+                <li>
+                    <a href="index.php?add_categories">Add categories</a>
+                </li>
+            </ul>
+        </div>
+        <div id="page-content-wrapper">
+            <div class="container-fluid m-3">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <?php 
+                            if(isset($_GET['add_categories'])){
+                                include('add_categories.php');
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
