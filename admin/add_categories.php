bf95a7fd14794cc01269383d69e66b31369c3f29
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2024-09-02 03:59:13 +0600

    Category_Inserted

--- admin/add_categories.php
+++ admin/add_categories.php
@@ -1,15 +1,32 @@
+<?php
+    include('../includes/connection.php');
+    if(isset($_POST['insert_category'])){
+        $category_title=$_POST['category_title'];
+        $select_query="Select * from `categories` where category_title='$category_title'";
+        $result_select=mysqli_query($conn,$select_query);
+        $number=mysqli_num_rows($result_select);
+        if($number>0){
+            echo "<script>alert('This category is already existed')</script>";
+        }
+        else {
+            $insert_query="insert into `categories` (category_title) values ('$category_title')";
+            $result=mysqli_query($conn,$insert_query);
+            if($result){
+                echo "<script>alert('Category inserted successfully')</script>";
+            }
+	    }
+    }
+?>
+
 <form action="" method="post" class="mb-2" >
     <div class="container-fluid text-center">
         <h3>Add Category</h3><br><br>
     </div>
-    <div class="container-fluid">
-        <div class="input-group bg-info w-90 mb-3 p-1">
-            <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-            <input type="text" class="form-control" name="category_title" placeholder="Insert Category" aria-label="insert_catgeory" aria-describedby="basic-addon1">
-        </div>
-        <div class="input-group w-10 mb-2 m-auto">
-            <button class="bg-info p-2 my-3 border-0">Add Category</button> 
-        </div>
+    <div class="input-group w-90 mb-3 p-1">
+        <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control" name="category_title" placeholder="Insert Category" aria-label="categories" aria-describedby="basic-addon1">
     </div>
-    
+    <div class="input-group w-10 mb-2 m-auto">
+        <input type="submit" class="bg-info border-0 p-2 my-3" name="insert_category"value="Insert Catgeory"> 
+    </div> 
 </form>
